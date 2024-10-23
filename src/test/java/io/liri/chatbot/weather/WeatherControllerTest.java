f08c81fd185794e213696a0c0ec54cbf872a2296
lirikoren <lirikoren57@gmail.com>
2024-10-23 20:02:56 +0300

    add weatherController test

---
+++ src/test/java/io/liri/chatbot/weather/WeatherControllerTest.java
@@ -0,0 +1,46 @@
+package io.liri.chatbot.weather;
+
+import io.liri.chatbot.weather.model.output.WeatherData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.ResponseEntity.ok;
+
+@ExtendWith(MockitoExtension.class)
+class WeatherControllerTest {
+
+    @Mock
+    WeatherFetcherService weatherFetcherService;
+    @Mock
+    WeatherData weatherData;
+    @InjectMocks
+    WeatherController weatherController;
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getWeather() {
+
+        Set<WeatherData> weatherDataSet = Collections.singleton(weatherData);
+        doReturn(weatherDataSet).when(weatherFetcherService).fetchWeatherData();
+        ResponseEntity<Set<WeatherData>> response = weatherController.getWeather();
+        assertEquals(ok(weatherDataSet), response);
+    }
+
+
+}
